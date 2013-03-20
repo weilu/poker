f743191ac48a758d36e6d9966bbbccf0ba4ab6fb
Wei Lu <luwei.here@gmail.com>
2013-03-20 13:53:42 +0800

    initial commit

---
+++ hand.rb
@@ -0,0 +1,35 @@
+class Hand
+  include Comparable
+  attr_reader :key_rank, :type_rank
+
+  def initialize cards
+    @key_rank, same_cards = cards.group_by{|i| card_rank(i)}.reduce do |memo, o|
+      maxRank = memo.last.count
+      currentRank = o.last.count
+      maxRank < currentRank || (maxRank == currentRank && o.first > memo.first) ? o : memo
+    end
+    @type_rank = same_cards.count
+  end
+
+  def <=> another_hand
+    if type_rank > another_hand.type_rank || (type_rank == another_hand.type_rank &&  key_rank > another_hand.key_rank)
+      1
+    elsif type_rank == another_hand.type_rank &&  key_rank == another_hand.key_rank
+      0
+    else
+      -1
+    end
+  end
+
+  def to_s
+    "type rank #{type_rank} and key card rank #{key_rank}"
+  end
+
+  private
+
+  def card_rank card
+    rank_map = ('1'..'13').to_a
+    card == '1' ?  13 : rank_map.index(card)
+  end
+end
+
---
+++ poker_game.rb
@@ -0,0 +1,26 @@
+require './hand'
+
+class PokerGame
+  def self.play input = $stdin, output = $stdout
+    begin
+      output.puts "Enter the hand of player 1:"
+      cards1 = input.gets
+      hand1 = Hand.new cards1.split(' ')
+
+      output.puts "Enter the hand of player 2:"
+      cards2 = input.gets
+      hand2 = Hand.new cards2.split(' ')
+
+      output.puts "Player 1 has a hand of #{hand1}."
+      output.puts "Player 2 has a hand of #{hand2}."
+
+      if hand1 == hand2
+        output.puts "There is a draw."
+      else
+        output.puts "Player #{hand1 > hand2 ? 1: 2} wins."
+      end
+    end while not input.gets.strip.empty?
+  end
+end
+
+#PokerGame.play
