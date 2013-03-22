9b014179d00722d6d2eeb838a53d870bb1f09cdc
Wei Lu <luwei.here@gmail.com>
2013-03-22 23:38:40 +0800

    remove some test cases

--- pp/about_poker.rb
+++ pp/about_poker.rb
@@ -3,34 +3,22 @@ require './hand'
 
 class AboutPoker < Test::Unit::TestCase
   def test_type_rank
-    assert_equal 4, type_rank([2, 2, 2, 2, 11])
-    assert_equal 4, type_rank([2, 2, 11, 2, 2])
     assert_equal 4, type_rank([2, 11, 2, 2, 2])
-
     assert_equal 3, type_rank([2, 2, 2, 11, 11])
-    assert_equal 3, type_rank([2, 11, 2, 2, 11])
-    assert_equal 3, type_rank([8, 8, 2, 2, 2])
-
-    assert_equal 2, type_rank([2, 2, 13, 9, 11])
-    assert_equal 2, type_rank([2, 11, 13, 2, 11])
     assert_equal 2, type_rank([2, 2, 11, 11, 8])
-
     assert_equal 1, type_rank([2, 3, 4, 5, 6])
   end
 
   def test_key_rank
     assert_equal 1, key_rank([2, 9, 9, 2, 2])
-    assert_equal 11, key_rank([12, 9, 8, 9, 12])
     assert_equal 11, key_rank([9, 12, 8, 9, 12])
     assert_equal 13, key_rank([1, 1, 9, 2, 2])
   end
 
   def test_compare_hands
     assert_equal 1, winner([2, 2, 2, 2, 11], [3, 3, 3, 1, 1])
-    assert_equal 2, winner([5, 11, 2, 4, 8], [9, 9, 1, 2, 3])
     assert_equal 2, winner([2, 2, 2, 2, 11], [13, 12, 12, 12, 12])
     assert_equal nil, winner([5, 11, 2, 4, 8], [8, 5, 11, 10, 7])
-    assert_equal 1, winner([9, 9, 11, 11, 5], [10, 10, 1, 2, 3])
   end
 end
 
